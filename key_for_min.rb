a5993d2bee9ca9c07e0fa508e01a86b15eb4a2af
elijahmm17 <elijahmm17@gmail.com>
2019-05-16 21:14:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   current_lowest = nil
   lowest_key = nil
   name_hash.each do |name, value|
-    if value < current_lowest || current_lowest == nil
+    if current_lowest == nil || value < current_lowest
       current_lowest = value
       lowest_key = name
     end
